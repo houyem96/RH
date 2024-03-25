42c7574306da495133a5d548dd94b2209f6050f7
houyem96 <houyembenamara96@gmail.com>
2024-03-25 14:38:48 +0100

    gestion_des_espaces_front

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  hide=false;
+  formulaire=false;
+  f_Et=false;
+  f_En=false;
+  loginAdministration(){
+    this.formulaire=true 
+  }
+  loginEtudiants(){
+    this.f_Et=true 
+  }
+  loginEnseignants(){
+    this.f_En=true 
+  }
+  fermer(){
+    this.formulaire=false;
+    this.f_Et=false;
+    this.f_En=false;
+  }
 }
